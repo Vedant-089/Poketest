ad84dbe3cd96660ddc73b90832d8c0569bc599ea
pokedia <pokedia47@gmail.com>
2026-04-03 16:00:53 +0530

    ok

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
 import asyncpg
 import asyncio
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Database:
     def __init__(self, dsn):
@@ -41,7 +45,7 @@ class Database:
 
 
 # Create a single global database instance
-db = Database("postgresql://postgres:pokedia_2389@db.urgwtnlyeezkbgbjlqjb.supabase.co:5432/postgres")
+db = Database(os.getenv("DATABASE_URL"))
 
 # Function to initialize the database at bot startup
 async def setup_database():
